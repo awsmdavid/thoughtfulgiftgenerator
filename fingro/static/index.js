fe99817ed114b1c5af0735aaedd12d0cba0a507d
awsmdavid <awsmdavid@gmail.com>
2014-12-10 02:15:43 -0500

    TODO: make button toggling actually toggle input value rather than just UI

--- fingro/static/index.js
+++ fingro/static/index.js
@@ -3,18 +3,44 @@ $(document).ready(function() {
   //gender input selector using images
   $('.gender_icon').click(function(){
     var $this = $(this);
+    var $this_class = $this.attr('class');
+    var $this_id = $this.attr('value');
+    //traverse node?
+    //if image is selected...
+    if ($(this).hasClass('on')) {
+        $(this).removeClass('on').addClass('off');
+    } else if ($(this).hasClass('off')) {
+        $(this).removeClass('off');
+    } else {
+        $(this).addClass('off');
+        alert($this.attr('class'));
+    }
     $('#gender_input').val($this.attr('value'));
   });
 
   //age input selector using images
   $('.age_icon').click(function(){
     var $this = $(this);
+    if ($(this).hasClass('on')) {
+        $(this).removeClass('on').addClass('off');
+    } else if ($(this).hasClass('off')) {
+        $(this).removeClass('off');
+    } else {
+        $(this).addClass('off');
+    }
     $('#age_input').val($this.attr('value'));
   });
 
   //gift category input selector using images
   $('.category_icon').click(function(){
     var $this = $(this);
+    if ($(this).hasClass('on')) {
+        $(this).removeClass('on').addClass('off');
+    } else if ($(this).hasClass('off')) {
+        $(this).removeClass('off');
+    } else {
+        $(this).addClass('off');
+    }
     $('#category_input_tech').val($this.attr('value'));
   });
 
