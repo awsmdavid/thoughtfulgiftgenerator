45181c2c4e7cef08e89fbecea00ab09dd7e9264a
awsmdavid <awsmdavid@gmail.com>
2014-12-10 01:10:25 -0500

    TODO: add toggle feature for categories

--- fingro/static/index.js
+++ fingro/static/index.js
@@ -1,33 +1,21 @@
 $(document).ready(function() {
-  $('img').on('click', function(){
-    var $this = $(this);
-    // var input_value = $this.attr('id');
-    //   alert ($this.attr('id'));
-    //   $this.val($this.attr('value'));
-    //   return false;
-    // });
-  });
-  // $('#female_icon').on('click', function(){
-  //   var gender_input = $('gender');
-  //   $('#female_icon').click(function(){
-  //       gender_input.val($(this).attr('value'));
-  //   });
-  // });
 
-  // $('#male_icon').on('click', function(){
-  //   alert("gender2 clicked");
-  //   return $('#gender1 select option[value="female"]').html();
-  // });
-
+  //gender input selector using images
   $('.gender_icon').click(function(){
-    var hinput = $(this);
-    $('#gender_input').val(hinput.attr('value'));
-    alert(hinput.attr('value'));
+    var $this = $(this);
+    $('#gender_input').val($this.attr('value'));
   });
 
-  // var hinput = $('input[name="gender]');
-  // $('.gender_icon').click(function(){
-  //     hinput.val('test');
-  //     alert(hinput.attr('value'));
-  // });
+  //age input selector using images
+  $('.age_icon').click(function(){
+    var $this = $(this);
+    $('#age_input').val($this.attr('value'));
+  });
+
+  //gift category input selector using images
+  $('.category_icon').click(function(){
+    var $this = $(this);
+    $('#category_input_tech').val($this.attr('value'));
+  });
+
 });
