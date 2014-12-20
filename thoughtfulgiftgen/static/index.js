7966bedad7842eda8f6088de73f18ce799c605e1
awsmdavid <awsmdavid@gmail.com>
2014-12-20 11:41:33 -0500

    updated UI with images made in powerpoint.. TODO: invest in photoshop

--- thoughtfulgiftgen/static/index.js
+++ thoughtfulgiftgen/static/index.js
@@ -45,16 +45,18 @@ $(document).ready(function() {
   $('.price_icon').click(function(){
     var $this = $(this);
     var $age_icon_class = $('.price_icon');
+    var aSource = $this.data('src');
+    var bSource = $this.data('alt-src');
     //if another input is already selected
     if ($(this).hasClass('off')){
-     $this.removeClass('off');
-     $this.addClass('on');
-     $age_icon_class.not("#"+$this.attr("id")).removeClass('on').addClass('off');
+      $this.removeClass('off');
+      $this.addClass('on');
+      $age_icon_class.not("#"+$this.attr("id")).removeClass('on').addClass('off');
     }
     //first time any option is selected (nothing has a class)
     else {
-     $age_icon_class.not("#"+$this.attr("id")).addClass('off');
-     $this.addClass('on');
+    $age_icon_class.not("#"+$this.attr("id")).addClass('off').attr('src');
+      $this.addClass('on');
     }
     $('#price_input').val($this.attr('value'));
   });
@@ -78,5 +80,16 @@ $(document).ready(function() {
     }
   });
 
+  var sourceSwap = function () {
+      var $this = $(this);
+      var newSource = $this.data('alt-src');
+      $this.data('alt-src', $this.attr('src'));
+      $this.attr('src', newSource);
+  };
 
+  $(function() {
+      $('img[data-alt-src]').each(function() {
+          new Image().src = $(this).data('alt-src');
+      }).hover(sourceSwap, sourceSwap);
+  });
 });
