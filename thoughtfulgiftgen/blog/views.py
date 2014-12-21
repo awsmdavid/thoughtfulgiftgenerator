ed7a746394339e7f027e741305fea30bde210985
awsmdavid <awsmdavid@gmail.com>
2014-12-20 20:50:42 -0500

    updated filter for view

--- thoughtfulgiftgen/blog/views.py
+++ thoughtfulgiftgen/blog/views.py
@@ -22,8 +22,44 @@ def results(request):
     	gifteeDataForm.fashion_flag = request.GET.get('fashion_flag')
     	gifteeDataForm.music_flag = request.GET.get('music_flag')
         gifteeDataForm.home_flag = request.GET.get('home_flag')
-        # fix this
-        gift_idea_result = GiftIdea.objects.filter(Q(tech_flag=gifteeDataForm.tech_flag) | Q(fitness_flag=gifteeDataForm.fitness_flag) | Q(travel_flag=gifteeDataForm.travel_flag) | Q(fashion_flag=gifteeDataForm.fashion_flag) | Q(music_flag=gifteeDataForm.music_flag)).filter(published=True, target_gender=gifteeDataForm.gender, target_age=gifteeDataForm.age).order_by('-upvote')[:1]
+        # if gender was answerd:
+        if request.GET.get('gender'):
+            if request.GET.get('age'):
+                if request.GET.get('price'):
+                    gift_idea_result = GiftIdea.objects.filter(
+                        Q(tech_flag=gifteeDataForm.tech_flag) | 
+                        Q(fitness_flag=gifteeDataForm.fitness_flag) | 
+                        Q(travel_flag=gifteeDataForm.travel_flag) | 
+                        Q(fashion_flag=gifteeDataForm.fashion_flag) | 
+                        Q(music_flag=gifteeDataForm.music_flag) |
+                        Q(home_flag=gifteeDataForm.home_flag)).filter(published=True, target_gender=gifteeDataForm.gender, target_age=gifteeDataForm.age, price=gifteeDataForm.price).order_by('-upvote')[:1]
+                else:
+                    gift_idea_result = GiftIdea.objects.filter(
+                        Q(tech_flag=gifteeDataForm.tech_flag) | 
+                        Q(fitness_flag=gifteeDataForm.fitness_flag) | 
+                        Q(travel_flag=gifteeDataForm.travel_flag) | 
+                        Q(fashion_flag=gifteeDataForm.fashion_flag) | 
+                        Q(music_flag=gifteeDataForm.music_flag) |
+                        Q(home_flag=gifteeDataForm.home_flag)).filter(published=True, target_gender=gifteeDataForm.gender, target_age=gifteeDataForm.age).order_by('-upvote')[:1]
+            else:
+                gift_idea_result = GiftIdea.objects.filter(
+                    Q(tech_flag=gifteeDataForm.tech_flag) | 
+                    Q(fitness_flag=gifteeDataForm.fitness_flag) | 
+                    Q(travel_flag=gifteeDataForm.travel_flag) | 
+                    Q(fashion_flag=gifteeDataForm.fashion_flag) | 
+                    Q(music_flag=gifteeDataForm.music_flag) |
+                    Q(home_flag=gifteeDataForm.home_flag)).filter(published=True, target_gender=gifteeDataForm.gender).order_by('-upvote')[:1]
+        else:
+            gift_idea_result = GiftIdea.objects.filter(
+                Q(tech_flag=gifteeDataForm.tech_flag) | 
+                Q(fitness_flag=gifteeDataForm.fitness_flag) | 
+                Q(travel_flag=gifteeDataForm.travel_flag) | 
+                Q(fashion_flag=gifteeDataForm.fashion_flag) | 
+                Q(music_flag=gifteeDataForm.music_flag) |
+                Q(home_flag=gifteeDataForm.home_flag)).filter(published=True).order_by('-upvote')[:1]
+  
+        # if gender was answerd:
+        gift_idea_result = GiftIdea.objects.filter(published=True).order_by('?')  
         # gift_idea_result = GiftIdea.objects.filter(published=True, target_gender=gifteeDataForm.gender, Q(tech_flag=gifteeDataForm.tech_flag) | Q(fitness_flag=gifteeDataForm.fitness_flag) | Q(travel_flag=gifteeDataForm.travel_flag) | Q(fashion_flag=gifteeDataForm.fashion_flag) | Q(music_flag=gifteeDataForm.music_flag)).order_by('-upvote')[:1]
         # for GiftIdea.object in gift_idea_result:
             # GiftIdea.object.match_rank = GiftIdea.object.calculate_rank(gifteeDataForm)
@@ -41,10 +77,3 @@ def random(request):
         # gift_idea_result = GiftIdea.objects.filter(Q(tags__icontains = search_term) | Q(description__icontains= search_term)).order_by('-likes')[:5]
         return render(request, 'blog/results.html', { 'gift_idea_result': gift_idea_result, 'gift_idea_secondary_results': gift_idea_secondary_results}) # Redirect after POST    # if request.POST: # If the form has been submitted...
     return render(request, 'blog/index.html')
-
-def gift(request):
-    # get the Post object
-    giftIdea = get_object_or_404(GiftIdea)
-    # widget_posts = Post.objects.filter(published=True).exclude(slug=slug).order_by('?')[:5]
-    # now return the rendered template
-    return render(request)
